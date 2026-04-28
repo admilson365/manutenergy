0aeec07cd8ee97c427a8183badac651ea2dcd979
admilson365 <admilson.dne@gmail.com>
2026-04-28 14:21:17 -0300

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -1,5 +1,5 @@
 import os
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
